053087199b58abfb734be05987fe76cf557d3ecb
Hesham maher <heshammaher@outlook.com>
2024-02-23 04:47:15 +0200

    Time: 47 ms (90.40%), Space: 49.5 MB (12.11%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let sum = init;
+    for (let i of nums){
+        sum = fn(sum,i)
+    }
+    return sum
+};
